20b4a327587aa2ba510ef942e33402d95ff6969f
DanielDlc <dlc.engcomputacao@gmail.com>
2020-10-28 21:35:21 -0300

    básico usando Dart

---
+++ básico/04_estrutura.dart
@@ -0,0 +1,11 @@
+main() {
+  var numero_01 = 3;
+  var numero_02 = 4;
+  var verdadeiro = numero_01 <= numero_02;
+
+  if(verdadeiro){
+    print("Informação verdadeira!");
+  } else {
+    print("Não é verdadeiro");
+  }
+}
